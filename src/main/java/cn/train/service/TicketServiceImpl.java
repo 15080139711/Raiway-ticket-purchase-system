f566986d3e0af8bb8fd55cf9de02315f6a24d9b0
xiaoning671 <614464386@qq.com>
2019-09-13 10:32:43 +0800

    查询单程列车接口修改--dn

--- src/main/java/cn/train/service/TicketServiceImpl.java
+++ src/main/java/cn/train/service/TicketServiceImpl.java
@@ -68,16 +68,14 @@ public class TicketServiceImpl implements TicketService {
     @Override
     public List<UnsoldTicket> SingleSearch(Search search) {
         System.out.println("开始查询车次...");
-        List<TrainInfo> temp = new ArrayList<TrainInfo>();
-        String day = search.getDay1();
-        temp = trainInfoMapper.getTrainByDay(day);
-        System.out.println(temp);
-        System.out.println("开始筛选车次...");
+        List<TrainInfo> temp = trainInfoMapper.getTrainByDay(search.getDay1());
+        System.out.println("开始筛选当日车次...");
         for(int i = 0; i < temp.size(); i++){
+            //该列车经停站信息tmp
             List<StopInfo> tmp = stopInfoMapper.getStopByTrainid(temp.get(i).getId());
-            System.out.println(tmp);
             int m1 = 0,m2 = 0;
             boolean n1 = false,n2 = false;
+            //判断该列车是否经过两个城市，m、n表示列车的站数
             for(int j = 0; j < tmp.size(); j++){
                 if(tmp.get(j).getCityid() == search.getCityid1()){
                     n1 = true;
@@ -93,16 +91,22 @@ public class TicketServiceImpl implements TicketService {
                 i--;
             }
         }
+        //上面得到的信息List temp是经过两个站的所有列车的信息
         System.out.println("查询完成！结果如下：");
         System.out.println(temp);
-        //开始生成回传的数据
+        //下面开始生成回传的数据
         List<UnsoldTicket> tickets = new ArrayList<UnsoldTicket>(temp.size());
+        //对每趟列车都生成一个UnsoldTicket型数据
         for(int i = 0; i < temp.size();i++){
             UnsoldTicket ticket = new UnsoldTicket();
+            //该列车的所有停站信息
             List<StopInfo> tmp = stopInfoMapper.getStopByTrainid(temp.get(i).getId());
             int[] path = new int[tmp.size()];
             int m = 0,n = 0;
+            //将所有的经停站的cityid生成一个path数组
             for(int j = 0; j<tmp.size(); j++){
+                //m、n表示要求的城市在数组的索引
+                //m出发站的索引，n到达站的索引
                 path[j] = tmp.get(j).getCityid();
                 if (tmp.get(j).getCityid() == search.getCityid1()){
                     m = j;
@@ -111,33 +115,38 @@ public class TicketServiceImpl implements TicketService {
                 }
             }
             System.out.println(m+" "+n);
+            //计算价格
             float price = CalculatePrice(path,m,n);
             ticket.setPrice(price);
+            //填充两个停站信息
             StopInfo a1 = new StopInfo();
-            CityInfo c1 = new CityInfo();
-            c1.setId(search.getCityid1());
-            c1.setName(cityInfoMapper.getNameByCityid(search.getCityid1()));
-            CityInfo c2 = new CityInfo();
-            c2.setId(search.getCityid2());
-            c2.setName(cityInfoMapper.getNameByCityid(search.getCityid2()));
-            a1.setCityInfo(c1);
-            a1.setLeave(tmp.get(m).getLeave());
             StopInfo a2 = new StopInfo();
-            a2.setArrive(tmp.get(n).getArrive());
+            for (int j = 0; j<tmp.size();j++){
+                if(tmp.get(j).getCityid() == search.getCityid1()){
+                    a1 = tmp.get(j);
+                }else if (tmp.get(j).getCityid() == search.getCityid2()){
+                    a2 = tmp.get(j);
+                }
+            }
+            //填充经停站的城市信息
+            CityInfo c1 = cityInfoMapper.selectByPrimaryKey(search.getCityid1());
+            CityInfo c2 = cityInfoMapper.selectByPrimaryKey(search.getCityid2());
+            a1.setCityInfo(c1);
             a2.setCityInfo(c2);
-            TrainInfo a3 = new TrainInfo();
-            a3.setNumber(temp.get(i).getNumber());
+//            TrainInfo a3 = new TrainInfo();
+//            a3.setNumber(temp.get(i).getNumber());
             ticket.setFromstop(a1);
             ticket.setTostop(a2);
-            ticket.setTrainInfo(a3);
+            ticket.setTrainInfo(temp.get(i));
 
-            //计算历时
+            //计算并填充历时
             String leave = tmp.get(m).getLeave();
             String arrive = tmp.get(n).getArrive();
             String time = Calculatetime(leave,arrive);
             System.out.println(time);
             ticket.setSeatnumber(time);
 
+            //完成一趟列车数据的填充，添加到返回值的列表中
             System.out.println(ticket);
             tickets.add(ticket);
         }
