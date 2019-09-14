b8eff47154c9ba673757b4bedd7479f11a7526bd
xiaoning671 <614464386@qq.com>
2019-09-14 23:10:47 +0800

    查漏补缺01--dn

--- src/main/java/cn/train/controller/TrainController.java
+++ src/main/java/cn/train/controller/TrainController.java
@@ -1,9 +1,11 @@
 package cn.train.controller;
 
 
+import cn.train.enity.MapTrainInfo;
 import cn.train.enity.Search;
 import cn.train.enity.UnsoldTicket;
 import cn.train.service.TicketService;
+import cn.train.service.TrainService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,11 +18,16 @@ public class TrainController {
 
     @Autowired
     TicketService ticketService;
+    @Autowired
+    TrainService trainService;
 
     @RequestMapping("/api/train/single")
     public List<UnsoldTicket> SingleSearch(@RequestBody Search search){
         return ticketService.SingleSearch(search);
     }
 
-
+    @RequestMapping("/api/train/getAllTrainInfo")
+    public List<MapTrainInfo> getAllTrainInfo(){
+        return trainService.getAllTrainInfo();
+    }
 }
---
+++ src/main/java/cn/train/service/TrainService.java
@@ -0,0 +1,11 @@
+package cn.train.service;
+
+import cn.train.enity.MapTrainInfo;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface TrainService {
+    List<MapTrainInfo> getAllTrainInfo();
+}
