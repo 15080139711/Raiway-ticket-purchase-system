5363b219b5428dbc1cf9c6ff6dd41f563c73132f
biubiuXiaoHai <1651736253@qq.com>
2019-09-18 16:25:37 +0800

    用redis实现了消息队列：在购票提交订单的时候，功能也就是不可能出现同一张票出现在两个用户的支付页面上。

--- src/main/java/cn/train/TestApplication.java
+++ src/main/java/cn/train/TestApplication.java
@@ -1,16 +1,32 @@
 package cn.train;
 
-import org.apache.ibatis.annotations.Mapper;
+//import cn.train.config.Consumer;
+import cn.train.config.Redis;
+import cn.train.controller.TicketController;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 @MapperScan("cn.train.mapper")
 public class TestApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
         SpringApplication.run(TestApplication.class, args);
+        Redis consumer=new Redis();
+        consumer.start();
+//        TicketController.receive();
+//        ConfigurableApplicationContext applicationContext = SpringApplication.run(TestApplication.class, args);
+//        //从 spring中取出已经有的bean
+//        CountDownLatch countDownLatch = applicationContext.getBean(CountDownLatch.class);
+//        StringRedisTemplate stringRedisTemplate = applicationContext.getBean(StringRedisTemplate.class);
+//
+//        stringRedisTemplate.convertAndSend(RedisConfig.MSG_TOPIC, "hello from jsbintask.");
+//
+//        // 一直等待消息被接收，没接收不退出
+//        countDownLatch.await();
+
     }
 
 }
