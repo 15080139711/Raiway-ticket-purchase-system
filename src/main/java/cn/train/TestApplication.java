b5e0c2c054ad84edfa7fc17743a3257433f9e396
biubiuXiaoHai <1651736253@qq.com>
2019-09-19 00:28:47 +0800

    最终版本1.0.1

--- src/main/java/cn/train/TestApplication.java
+++ src/main/java/cn/train/TestApplication.java
@@ -16,17 +16,6 @@ public class TestApplication {
         SpringApplication.run(TestApplication.class, args);
         Redis consumer=new Redis();
         consumer.start();
-//        TicketController.receive();
-//        ConfigurableApplicationContext applicationContext = SpringApplication.run(TestApplication.class, args);
-//        //从 spring中取出已经有的bean
-//        CountDownLatch countDownLatch = applicationContext.getBean(CountDownLatch.class);
-//        StringRedisTemplate stringRedisTemplate = applicationContext.getBean(StringRedisTemplate.class);
-//
-//        stringRedisTemplate.convertAndSend(RedisConfig.MSG_TOPIC, "hello from jsbintask.");
-//
-//        // 一直等待消息被接收，没接收不退出
-//        countDownLatch.await();
-
     }
 
 }
